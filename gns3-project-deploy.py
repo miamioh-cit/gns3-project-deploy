ef9970c83566d6fee7aea946f2c295d37e3fac43
Anthony W Rose <83881859+miamioh-roseaw@users.noreply.github.com>
2025-03-19 15:24:24 -0400

    Update gns3-project-deploy.py

--- gns3-project-deploy.py
+++ gns3-project-deploy.py
@@ -5,7 +5,7 @@ import logging
 # Enable debug logging
 logging.basicConfig(level=logging.DEBUG)
 
-LAB_NAME = "281-new"
+LAB_NAME = "281-stuff"
 
 # Base IP address (first three octets remain constant)
 BASE_IP = "http://10.48.229."
