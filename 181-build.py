fcffa4b83e65c84f76143be4d5a9412552974d58
mcclela <160270505+mcclela@users.noreply.github.com>
2025-05-31 13:01:39 -0400

    Create 181-build.py

---
+++ 181-build.py
@@ -0,0 +1,60 @@
+import logging
+from gns3fy import Gns3Connector, Project, Node, Link
+
+LAB_NAME = "281-test21"
+
+BASE_IP = "http://10.48.229."
+
+# Read last octets from datastore file
+try:
+    with open("datastore", "r") as f:
+        content = f.read().strip()
+        SERVER_LAST_OCTETS = [int(octet.strip()) for octet in content.split(",") if octet.strip().isdigit()]
+except Exception as e:
+    print("Error reading datastore file:", e)
+    SERVER_LAST_OCTETS = []
+
+if not SERVER_LAST_OCTETS:
+    raise ValueError("No valid server last octets found in 'datastore'.")
+
+SERVER_URLS = [f"{BASE_IP}{octet}:80" for octet in SERVER_LAST_OCTETS]
+
+GNS3_USER = "gns3"
+GNS3_PW = "gns3"
+
+for SERVER_URL in SERVER_URLS:
+    server = Gns3Connector(url=SERVER_URL, user=GNS3_USER, cred=GNS3_PW)
+    print("Connecting to GNS3 server to verify uniqueness of Project name", server.get_version(), "at", SERVER_URL)
+
+    lab = server.create_project(name=LAB_NAME)
+
+    print("-----------------------------------------------------------------------")
+    print(f"Project '{LAB_NAME}' created on {SERVER_URL}. Nodes are being created.")
+    print("-----------------------------------------------------------------------")
+    print("Please wait until script runs before entering the project in GNS3!")
+    print("-----------------------------------------------------------------------")
+
+    lab = Project(name=LAB_NAME, connector=server)
+    lab.get()
+    lab.open()
+
+    available_templates = [template["name"] for template in server.get_templates()]
+    logging.debug(f"Available Templates: {available_templates}")
+
+    lab.create_node(name='internet', template='Cloud', x=76, y=-76)
+
+    lab.create_node(name='offsite-switch', template='Cisco IOSvL2 15.2.1', x=-33, y=-175)
+    sw1 = lab.get_node("offsite-switch")
+    sw1.start()
+
+
+
+    lab.create_link("offsite-web", "Ethernet0", "offsite-switch", "Gi0/0")
+  
+
+    print("-----------------------------------------------------------------------")
+    print("Nodes created, started and linked. Here are the links:")
+    print("-----------------------------------------------------------------------")
+    lab.links_summary()
+    print("-----------------------------------------------------------------------")
+    print(LAB_NAME + f" build is Complete on {SERVER_URL}. It is now safe to open the project in GNS3")
