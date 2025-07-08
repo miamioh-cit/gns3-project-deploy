a7165a75b32b2c13d6f67b86c93f444f5b7ad2a4
mcclela <160270505+mcclela@users.noreply.github.com>
2025-07-08 17:34:24 -0400

    Update 325-build.py

--- 325-build.py
+++ 325-build.py
@@ -93,7 +93,15 @@ for SERVER_URL in SERVER_URLS:
 
 
 
-    lab.create_link("offsite-web", "Ethernet0", "offsite-switch", "Gi0/0")
+    lab.create_link("mgmt", "Gi0/0", "1", "Ethernet0")
+    lab.create_link("mgmt", "Gi0/1", "isp", "eth0")
+    lab.create_link("1", "Ethernet1", "oxford", "Ethernet0")
+    lab.create_link("1", "Ethernet2", "mid-I", "Ethernet0")
+    lab.create_link("1", "Ethernet3", "mid-w", "Ethernet0")
+    lab.create_link("1", "Ethernet4", "mid-r", "Ethernet0")
+    lab.create_link("1", "Ethernet5", "ham-r", "Ethernet0")
+    lab.create_link("1", "Ethernet6", "ham-w", "Ethernet0")
+    lab.create_link("1", "Ethernet7", "ham-I", "Ethernet0")
 
 
     print("-----------------------------------------------------------------------")
