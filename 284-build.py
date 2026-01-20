7c5d1725f13ec345ce2ea98c5417e5d68b24a945
Anthony W Rose <83881859+miamioh-roseaw@users.noreply.github.com>
2026-01-20 12:04:27 -0500

    Update 284-build.py

--- 284-build.py
+++ 284-build.py
@@ -46,14 +46,6 @@ for SERVER_URL in SERVER_URLS:
     ADDC = lab.get_node("ADDC")
     ADDC.start()
 
-    lab.create_node(name='Windows_Client-1', template='Windows 11', x=-401, y=58)
-    win_1 = lab.get_node("Windows_Client-1")
-    win_1.start()
-
-    lab.create_node(name='Windows_Client-2', template='Windows 11', x=-559, y=58)
-    win_2 = lab.get_node("Windows_Client-2")
-    win_2.start()
-
     lab.create_node(name='Switch1', template='Ethernet switch', x=-472, y=-30)
     switch1 = lab.get_node("Switch1")
     switch1.start()
@@ -61,6 +53,14 @@ for SERVER_URL in SERVER_URLS:
     lab.create_node(name='NAT1', template='NAT', x=-512, y=-310)
     nat1 = lab.get_node("NAT1")
     nat1.start()
+
+    lab.create_node(name='Windows_Client-1', template='Windows 11', x=-401, y=58)
+    win_1 = lab.get_node("Windows_Client-1")
+    win_1.start()
+
+    lab.create_node(name='Windows_Client-2', template='Windows 11', x=-559, y=58)
+    win_2 = lab.get_node("Windows_Client-2")
+    win_2.start()
 
     #Create links  
     
