87c77f398a9c3f99fc06b44997c9f96867784649
Anthony W Rose <83881859+miamioh-roseaw@users.noreply.github.com>
2026-08-08 10:33:49 -0400

    Update link name from 'e0' to 'Ethernet0'

--- 480-build.py
+++ 480-build.py
@@ -283,7 +283,7 @@ for SERVER_URL in SERVER_URLS:
 
 
 # --- Top Vlan-01 Segment ---
-    lab.create_link("PLC-Influent", "eth0", "Vlan-01", "e0")
+    lab.create_link("PLC-Influent", "eth0", "Vlan-01", "Ethernet0")
     lab.create_link("FT-101", "eth0", "Vlan-01", "Ethernet1")
     lab.create_link("LT-101", "eth0", "Vlan-01", "Ethernet2")
     lab.create_link("DP-101", "eth0", "Vlan-01", "Ethernet3")
