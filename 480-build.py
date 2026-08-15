760bebc6a35af6db91634b595e02142b11d09466
EvanKunkel <kunkelec@miamioh.edu>
2026-08-14 21:07:20 -0400

    Update 480-build.py

--- 480-build.py
+++ 480-build.py
@@ -44,14 +44,12 @@ for SERVER_URL in SERVER_URLS:
     try:
         lab.create_node(name='H-101', template='generic-sensor', x=-575, y=-625)
         sw1 = lab.get_node("H-101")
-        sw1.start()
     except Exception as e:
         print(f"Error creating or starting node: {e}")
     
     try:
         lab.create_node(name='LT-101', template='generic-sensor', x=-483, y=-628)
         sw2 = lab.get_node("LT-101")
-        sw2.start()
     except Exception as e:
         print(f"Error creating or starting node: {e}")
 
