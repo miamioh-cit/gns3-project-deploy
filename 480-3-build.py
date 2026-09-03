5bcac13e43b4159f34e9f74be8487ff5778098ca
Anthony W Rose <83881859+miamioh-roseaw@users.noreply.github.com>
2026-09-03 13:49:42 -0400

    Update LAB_NAME to 'Miami Valley Traffic Operations'

--- 480-3-build.py
+++ 480-3-build.py
@@ -2,7 +2,7 @@ import logging
 import requests
 from gns3fy import Gns3Connector, Project, Node, Link
 
-LAB_NAME = "CIT480 Water Sewage Treatment Plant"
+LAB_NAME = "Miami Valley Traffic Operations"
 
 BASE_IP = "http://10.48.229."
 
