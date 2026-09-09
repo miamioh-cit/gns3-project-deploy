02d6a643d31c976fd1ce8c966bc6cbcc6f102b32
EvanKunkel <kunkelec@miamioh.edu>
2026-09-09 15:31:42 -0400

    Update 480-3-build.py

--- 480-3-build.py
+++ 480-3-build.py
@@ -758,7 +758,7 @@ def scada_environment():
     return build_environment(
         SCENARIO=SCENARIO,
         SCADA_SUBNETS=OPERATIONS_SUBNET,
-        SCADA_DIAGRAM_CONFIG="module3/diagrams.yaml",
+        SCADA_DIAGRAM_CONFIG="traffic/diagrams.yaml",
         IP_ADDRESS=SCADA_IP,
         NETMASK=OPERATIONS_NETMASK,
     )
