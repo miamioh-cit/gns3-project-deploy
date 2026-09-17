b223bdda4f9ffd02794889b00a27e6f3f52deca3
EvanKunkel <kunkelec@miamioh.edu>
2026-09-17 14:43:05 -0400

    Update 480-4-build.py

--- 480-4-build.py
+++ 480-4-build.py
@@ -1243,16 +1243,13 @@ def historian_environment() -> str:
 
 
 def scada_environment() -> str:
-
     return build_environment(
-
         SCENARIO=SCENARIO,
-
         SCADA_SUBNETS=OPERATIONS_SUBNET,
-
+        SCADA_DIAGRAM_CONFIG="manufacturing/diagrams.yaml",
         IP_ADDRESS=SCADA_IP,
-
         NETMASK=OPERATIONS_NETMASK,
+    )
     )
 
 
